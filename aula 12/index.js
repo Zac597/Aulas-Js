13d4820b55a3c1664a654110fcdb145de4c3e8a5
Zac597 <isaaclima597@gmail.com>
2022-06-29 18:10:59 -0300

    Add ex aula 13

--- aula 12/index.js
+++ aula 12/index.js
@@ -1,9 +1,15 @@
 let string = "Um \"texto\"";
 let string2 = "Um \\texto";
 let string3 = "Um texto";
+let string4 = "O Rato Roeu a Roupa do Rei de Roma."
 
-console.log(string);
+/*console.log(string);
 console.log(string2)
 console.log(string3[1])
 console.log(`${string3} em um lindo dia`);
 console.log(string3.lastIndexOf('t', 4));
+console.log(string4.replace(/R/g, '#')) substituir 
+console.log(string4.substring(string4.length -5, string4.length -1)); // length quantos caracteres tem 
+console.log(string4.split(' ', 3));
+console.log(string4.toUpperCase()); // tolowercase */   
+console.log(string4.substring(string4.length -5));
