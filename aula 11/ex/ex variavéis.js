66d916e596df63320717422a1edeaf8f9982db92
Zac597 <isaaclima597@gmail.com>
2022-06-29 17:07:12 -0300

    Att

---
+++ aula 11/ex/ex variavéis.js
@@ -0,0 +1,16 @@
+let varA = 'A'; // B
+let varB = 'B'; // C 
+let varC = 'C'; // A
+
+/*
+const varD = varA;
+varA = varB
+varB = varC
+varC = varD
+*/
+
+/*
+[varA, varB, varC] = [varB, varC, varA]
+*/
+
+console.log(varA, varB, varC)
---
+++ aula 12/index.js
@@ -0,0 +1,9 @@
+let string = "Um \"texto\"";
+let string2 = "Um \\texto";
+let string3 = "Um texto";
+
+console.log(string);
+console.log(string2)
+console.log(string3[1])
+console.log(`${string3} em um lindo dia`);
+console.log(string3.lastIndexOf('t', 4));
