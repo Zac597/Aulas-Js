1c9645db4e4a1c3901751f54f2aa65a3967b08ad
Zac597 <isaaclima597@gmail.com>
2022-07-16 18:53:47 -0300

    Aula 14 e 15

---
+++ aula 14/index.js
@@ -0,0 +1,15 @@
+// IEEE 754-2008
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
+
+console.log(num1);
+
+//console.log(num1.toString() + num2);
+//console.log(num1.toString(2))
+//console.log(num1.toFixed(2));
+//console.log(Number.isInteger(num1));
+//console.log(Number.isNaN(temp));
+//let temp = num1 * '5';
+//console.log(temp)
